cbb7352274edbe3eac0a265588476769665f3ed8
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-06-21 19:41:01 +0900

    qwe

--- Maru-Android/Maru/presentation/src/main/java/com/traveling/presentation/features/main/MainActivity.kt
+++ Maru-Android/Maru/presentation/src/main/java/com/traveling/presentation/features/main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.traveling.presentation.features.main
 
 import androidx.activity.viewModels
+import com.traveling.presentation.R
 import com.traveling.presentation.base.BaseActivity
 import com.traveling.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -9,5 +10,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
     override val viewModel: MainViewModel by viewModels()
     override fun observerViewModel() {
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initToolBar()
+    }
+    private fun initToolBar() {
+        setSupportActionBar(mBinding.toolBar)
+        with(supportActionBar!!) {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+            setDisplayShowTitleEnabled(false)
+        }
     }
 }
