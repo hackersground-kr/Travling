b185f14686c28572949176ebda9d15fd7f6f5ba7
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-06-21 15:39:27 +0900

    module

---
+++ Maru-Android/data/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.maru"
+
+    defaultConfig {
+        compileSdk = 33
+        minSdk = 28
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+dependencies {
+    implementation ("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("com.google.dagger:hilt-android:2.44.2")
+    kapt("com.google.dagger:hilt-android-compiler:2.44.2")
+
+    implementation ("androidx.core:core-ktx:1.8.0")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation (project(":domain"))
+}
