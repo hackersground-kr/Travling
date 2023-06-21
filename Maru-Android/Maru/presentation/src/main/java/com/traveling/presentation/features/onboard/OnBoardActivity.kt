4e723caa3ebf68709263193530e42ff86bb70a0d
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-06-21 19:15:45 +0900

    qwe
    weqwe

--- Maru-Android/Maru/presentation/src/main/java/com/traveling/presentation/features/onboard/OnBoardActivity.kt
+++ Maru-Android/Maru/presentation/src/main/java/com/traveling/presentation/features/onboard/OnBoardActivity.kt
@@ -1,9 +1,16 @@
 package com.traveling.presentation.features.onboard
 
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.traveling.presentation.base.BaseActivity
+import com.traveling.presentation.databinding.ActivityOnBoardBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class OnBoardActivity : AppCompatActivity() {
+class OnBoardActivity : BaseActivity<ActivityOnBoardBinding, OnBoardViewModel>() {
+    override val viewModel: OnBoardViewModel by viewModels()
+    override fun observerViewModel() {
+
+    }
 
 }
