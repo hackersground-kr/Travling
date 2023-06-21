988a691dffac5ec7a66f4442af4c98b189f0ce09
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-06-21 17:55:30 +0900

    asd

--- Maru-Android/Maru/presentation/src/main/java/com/traveling/presentation/features/onboard/start/StartFragment.kt
+++ Maru-Android/Maru/presentation/src/main/java/com/traveling/presentation/features/onboard/start/StartFragment.kt
@@ -1,13 +1,14 @@
-package com.traveling.presentation.features.onboard.health
+package com.traveling.presentation.features.onboard.start
 
 import androidx.fragment.app.viewModels
 import com.traveling.presentation.base.BaseFragment
 import com.traveling.presentation.databinding.FragmentHealthBinding
+import com.traveling.presentation.databinding.FragmentStartBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HealthFragment : BaseFragment<FragmentHealthBinding, HealthViewModel>() {
-    override val viewModel: HealthViewModel by viewModels()
+class StartFragment : BaseFragment<FragmentStartBinding, StartViewModel>() {
+    override val viewModel: StartViewModel by viewModels()
     override fun observerViewModel() {
 
     }
