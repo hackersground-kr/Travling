f2db92c0aedfb2156a0993120aea97bddd5032de
8954sood <58807462+8954sood@users.noreply.github.com>
2023-06-22 13:23:29 +0900

    fixed error

--- Maru-Android/Maru/presentation/src/main/java/com/traveling/presentation/features/lock/LockViewModel.kt
+++ Maru-Android/Maru/presentation/src/main/java/com/traveling/presentation/features/lock/LockViewModel.kt
@@ -3,9 +3,11 @@ package com.traveling.presentation.features.lock
 import com.traveling.presentation.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import java.util.Calendar
+import javax.inject.Inject
 
 @HiltViewModel
-class LockViewModel: BaseViewModel() {
+class LockViewModel @Inject constructor()
+    : BaseViewModel() {
 
 
 }
