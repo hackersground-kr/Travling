60329b23d93e41da0764332b935b8b7b9e62628d
bswlkh0310 <hhhello0507@dgsw.hs.kr>
2023-06-23 00:37:09 +0900

    12

--- Maru/presentation/src/main/java/com/traveling/presentation/features/lock/screen/ScreenFragment.kt
+++ Maru/presentation/src/main/java/com/traveling/presentation/features/lock/screen/ScreenFragment.kt
@@ -62,6 +62,4 @@ class ScreenFragment: BaseFragment<FragmentScreenBinding, ScreenViewModel>() {
     private fun isFragmentActive(): Boolean {
         return isAdded && !isDetached && !isRemoving
     }
-
-
 }
